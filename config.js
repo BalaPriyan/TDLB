48713c3f3dfc8c84dfbda6421a7b838b56f27813
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-04-11 18:38:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // const tgToken = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"; //aryantestbot
-const tgToken = "1237067579:AAGIFKPpyPk9FGD55SpKN6CgZaiW8iEj2Io"; //gtorrent
+const tgToken = "6889408345:AAHmJ-bK90E7Y3-ap2bIXOkgLHX5uuUc3QU"; //gtorrent
 const downloadFolder = "downloads";
-const mongoUrl = "mongodb+srv://aryanvikash:vikash12@cluster0-mp4rw.mongodb.net/test?retryWrites=true&w=majority"
+const mongoUrl = "mongodb+srv://fino:fino@cluster0.ko0stef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // const maxSize = 10;
 
 module.exports = { tgToken, downloadFolder, mongoUrl };
